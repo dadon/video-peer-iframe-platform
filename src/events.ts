c757f6da90382e16873257b755df409e9b9df1d3
Dmitry Zvada <dmitryzvada@gmail.com>
2020-04-24 00:19:00 +0300

    use enums for events

--- src/events.ts
+++ src/events.ts
@@ -1,9 +1,9 @@
-export const PlatformEvents = {
-    APP_INIT: "APP_INIT",
-    UPDATE_STATE: "UPDATE_STATE",
-};
+export enum PlatformEvents {
+    APP_INIT = "APP_INIT",
+    UPDATE_USERS_STATE = "UPDATE_USERS_STATE",
+}
 
-export const AppEvents = {
-    APP_READY: "APP_READY",
-    SET_USER_STATE: "SET_USER_STATE",
-};
+export enum AppEvents {
+    APP_READY = "APP_READY",
+    SET_USER_STATE = "SET_USER_STATE",
+}
